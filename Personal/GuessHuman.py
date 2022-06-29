ffba0c38404706b213802a932ce692212745bb72
Ajay T Shaju <ajaytshaju@gmail.com>
2022-06-30 00:41:38 +0530

    comments added

--- Personal/GuessHuman.py
+++ Personal/GuessHuman.py
@@ -2,19 +2,20 @@
 
 import random
 def guess(num):
-    random_num = random.randint(1, num)
-    guess = 0
+    random_num = random.randint(1, num) # computer takes a random number for human to guess
+    guess = steps = 0
     while guess != random_num:
-        guess = int(input(f'Guess a num b\w 1 & {num}: '))
+        steps += 1 # incrementing step count
+        guess = int(input(f'Guess a num b\w 1 & {num}: ')) # getting guess number
         if guess < random_num:
             print("Guess is too low")
         elif guess > random_num:
             print("Guess is too high")
-    print(f'Congrats...You correctly guessed {random_num}')        
+    print(f'Congrats...You correctly guessed {random_num}\nNumber of steps: {steps-1}') # steps - 1 for omitting the false iteration of loop        
 
 while True:
-    inp_num = int(input("Enter a num(limit): "))
-    guess(inp_num)
-    if input("Again? y/n: ") != 'y':
+    limit = int(input("Enter a num(limit): "))
+    guess(limit)
+    if input("Again? y/n: ") != 'y': # For breaking while loop
         print('See you again, exited.')
         break
