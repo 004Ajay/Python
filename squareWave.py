cd1f961db5594d8393b4a975548157943447e5e0
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-25 21:46:37 +0530

    comment

--- squareWave.py
+++ squareWave.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
-x = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6]
+# values of x & y are made using trial & error method
+x = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6] 
 y = [0, 1, 1, 0, -1, -1, 1, 1, -1, -1, 1, 1, 0, 0]
 plt.plot(x, y)
 plt.title('Square Wave')
