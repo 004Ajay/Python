1122a98c34cef5bf2b63348c7caf6b83cce996a9
Ajay T Shaju <ajaytshaju@gmail.com>
2022-07-25 21:59:31 +0530

    new

--- Factorial.py
+++ Factorial.py
@@ -1,6 +1,6 @@
 # Python program to find factorial of a number
 
-num = int(input("Enter a number: "))
+num = int(input("Enter a number: ")
 fact = 1
 for i in range(1,num + 1):
     fact = fact * i
