069ccfbddf93f9d7f571e5762e6ae4b2f22edf4f
Ajay T Shaju <ajaytshaju@gmail.com>
2022-08-03 01:26:41 +0530

    comments

--- Personal/AlphaSort.py
+++ Personal/AlphaSort.py
@@ -1,2 +1,7 @@
+"""
+Example:
+Input: w o r d <each letter separated by a single space>
+Output: d o r w
+"""
 inp = input("Enter a word separated by spaces in each alphabet: ").split(" ")
 print(" ".join(sorted(inp)),end="")
